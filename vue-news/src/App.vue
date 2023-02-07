65e16d63c1e7644043fdfbe865149db947af857d
김상희 <kimsanghee@reflexion.co.kr>
2023-02-07 15:52:42 +0900

    ItemView와 UserView 생성 완료

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -16,5 +16,7 @@ export default {
 </script>
 
 <style>
-
+body{
+  padding:0; margin:0;
+}
 </style>
--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -1,17 +1,22 @@
 <template>
-    <div>
-        <router-link to="/news">news</router-link>
-        <router-link to="/ask">ask</router-link>
+    <div class="header">
+        <router-link to="/news">news</router-link> |
+        <router-link to="/ask">ask</router-link> |
         <router-link to="/jobs">jobs</router-link>
     </div>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+.header{
+    color:white;
+    background: #42b883;
+    display: flex; 
+    padding:8px;
 }
-</script>
-
-<style>
-
+.header .router-link-exact-active{
+    color:#35495e; text-decoration: none;
+}
+.header a{
+    color:white; text-decoration: none; padding: 0 10px;
+}
 </style>
