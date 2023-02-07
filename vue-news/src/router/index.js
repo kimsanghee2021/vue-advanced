de750d6cd24016e989bfb46bbc99915c1f8c0e03
김상희 <kimsanghee@reflexion.co.kr>
2023-02-07 14:44:56 +0900

    라우터 뷰 생성

--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -1,31 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
 
-import NewsView from '../views/NewsView';
-import AskView from '../views/AskView';
-import JobsView from '../views/JobsView';
 
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-const router = new VueRouter({
+export const router = new VueRouter({
     mode: 'history',
     routes : [
         {
             //path : url 주소
             // component : url 주소로 갔을 때 표시될 컴포넌트
-            path :'./news',
+            path :'/news',
             component : NewsView
         },
         {
-            path :'./ask',
+            path :'/ask',
             component : AskView
         },
         {
-            path :'./jobs',
+            path :'/jobs',
             component : JobsView
         }
     ]
 })
-
-export default router
