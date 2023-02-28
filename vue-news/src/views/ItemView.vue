bc95265f3d6be32755f60307493f0516e1373bdc
김상희 <kimsanghee@reflexion.co.kr>
2023-02-28 11:08:20 +0900

    실습 순서 경로

---
+++ vue-news/src/views/ItemView.vue
@@ -0,0 +1,22 @@
+<template>
+
+<div>
+
+</div>   
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+/*
+    실습 순서 
+    1.ItemView 생성
+    2. 라우터 ItemView로 갈 수 있는 라우터 정보를 등록
+    3. 해당 컴포넌트로 이동 했을 때 받아온 params(id)를 이용해서 페이지 데이터를 표시
+    4. 가져온 데이터를 해당 페이지에 뿌려줘야한다. 
+*/
+</style>
